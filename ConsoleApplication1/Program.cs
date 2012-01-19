628b2f25e1a809f1ff70fd4d6fa7227c93eb646a
choko93 <beto.ok93@gmail.com>
2012-01-19 06:15:32 -0600

    First push

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "";
+            Console.WriteLine("ESCRIBA Y PARA CONTINUAR");
+            do
+            {
+                s = Console.ReadLine();
+                if (s.Equals("y") == false) { Console.WriteLine("ERROR"); }
+            }
+            while (s.Equals("y") == false);
+            
+            
+        }
+    }
+}
