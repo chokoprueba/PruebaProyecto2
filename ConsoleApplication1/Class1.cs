6a629c9e441005c86a724c4492dff5e2eb1b7cb7
choko93 <beto.ok93@gmail.com>
2012-01-19 06:39:00 -0600

    third push

--- ConsoleApplication1/Class1.cs
+++ ConsoleApplication1/Class1.cs
@@ -7,5 +7,22 @@ namespace ConsoleApplication1
 {
     class Class1
     {
+        
+        public Class1() {
+         
+        }
+
+        
+
+        public Boolean esPar(int n) {
+
+            int s = 0;
+            s = n % 2;
+            if (s == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
     }
 }
